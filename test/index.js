7c11f5aa3dc49c000b58d78b4c28d2f217d1fa14
Matthew Conover <mconover@mediamath.com>
2017-06-01 16:48:13 -0400

    simplifications

--- test/index.js
+++ test/index.js
@@ -3,20 +3,23 @@ const connect = require('connect');
 const http = require('http');
 const path = require('path')
 const proxy = require('http-proxy-middleware');
-const serveStatic = require('serve-static');
 const url = require('url');
 const bodyParser = require('body-parser');
-const adamaProxy = require('../index.js')
-
+const adamaProxy = require('../index.js');
 
 const app = connect();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json())
-app.use(serveStatic(path.join(__dirname, '/dist')))
 app.use(adamaProxy);
 
 // beanstalk default: 8081
 http.createServer(app).listen(8081);
-console.info('created app at: 8081');
+console.info('created test at: 8081');
+console.info('open a browser and go to: localhost:8081/api/v2.0/session');
+console.info('then to: localhost:8081/api/v2.0/login');
+console.info('then to: localhost:8081/api/v2.0/session');
+console.info('then to: localhost:8081/api/v2.0/campaigns');
+console.info('etc etc');
+console.info('if your .env file has a T1USER, T1PASSWORD, T1APIKEY everything should work');
