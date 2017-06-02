4fa806b77004537bbc4ca86895e7b0a90708df23
Matthew Conover <matthew@Samson.local>
2017-06-01 23:02:56 -0400

    added NODE_ENV checks

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const proxy = require('http-proxy-middleware');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -9,12 +10,6 @@ function credentials() {
   creds.user = process.env.T1USER;
   creds.password = process.env.T1PASSWORD;
   creds.api_key = process.env.T1APIKEY;
-  
-  Object.keys(creds).forEach((key) => {
-    if (creds[key].length == 0) {
-      throw new Error('T1 Credentials not present in `.env`. Credentials are: T1USER, T1PASSWORD, and T1APIKEY');
-    }
-  })
 
   return creds;
 }
@@ -28,8 +23,26 @@ const adamaProxy = proxy(['/api/v2.0', '/media/v1.0'], {
   onProxyReq(proxyReq, req) {
     let creds = credentials();
 
-    if (req.url === '/api/v2.0/login') {
+    if (req.url === '/api/v2.0/login' && req.method === 'POST') {
+      // assume the request is from a login form
+      creds.user = req.body.user;
+      creds.password = req.body.password;
+    }
+
+    if (req.url === '/api/v2.0/login' &&
+        req.method === 'GET' &&
+        process.env.NODE_ENV.match(/dev/)) {
+      // local dev login route.
+      // Running in NODE_ENV=dev and with a proper .env file will put an Adama
+      // session cookie in the browser that makes the request to /api/v2.0/login
+      Object.keys(creds).forEach((key) => {
+        if (!creds[key] || creds[key].length == 0) {
+          console.error(`T1 Credentials missing from \`.env\`. Credentials are: T1USER, T1PASSWORD, T1APIKEY`);
+        }
+      })
+
       proxyReq.method = 'POST';
+
     }
 
     delete req.body;
